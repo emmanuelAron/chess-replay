2e1492e8c5ca3349cb6f7223642e887804f1a686
Emmanuel Aron <emmanuel.aron@gmail.com>
2026-01-29 00:07:24 +0100

    Add Kafka replay test endpoint and config

--- chess-replay-v1/src/main/java/com/github/emmanuelAron/config/KafkaConfig.java
+++ chess-replay-v1/src/main/java/com/github/emmanuelAron/config/KafkaConfig.java
@@ -1,11 +1,4 @@
 package com.github.emmanuelAron.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Profile("local")
-@Configuration
-@EnableKafka
 public class KafkaConfig {
 }
---
+++ chess-replay-v1/src/main/java/com/github/emmanuelAron/controller/ReplayTestController.java
@@ -0,0 +1,4 @@
+package com.github.emmanuelAron.controller;
+
+public class ReplayTestController {
+}
